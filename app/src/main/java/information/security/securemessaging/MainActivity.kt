a181a9be6eabfb4010bd652493eed96db59ea007
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-11-22 21:56:54 -0600

    Add placeholder backend methods and attributes

--- app/src/main/java/information/security/securemessaging/MainActivity.kt
+++ app/src/main/java/information/security/securemessaging/MainActivity.kt
@@ -1,20 +1,58 @@
 package information.security.securemessaging
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import information.security.securemessaging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAlgorithmAndKeySizeSelector()
+        setupButtons()
+    }
+
+    private fun setupAlgorithmAndKeySizeSelector() {
+        binding.algorithmSelector.setOnCheckedChangeListener { _, _ ->
+            updateKeySizeSpinner()
+        }
+        // Initial setup
+        updateKeySizeSpinner()
+    }
+
+    private fun updateKeySizeSpinner() {
+        val keySizes = if (binding.rsaButton.isChecked) {
+            arrayOf("1024", "2048")
+        } else {
+            arrayOf("256", "384", "521")
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, keySizes)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.keySizeSelector.adapter = adapter
+    }
+
+    private fun setupButtons() {
+        binding.encryptButton.setOnClickListener {
+            log("Encrypt & Send button clicked")
+        }
+
+        binding.decryptButton.setOnClickListener {
+            log("Decrypt button clicked")
+        }
+    }
+
+    private fun log(message: String) {
+        val currentLog = binding.logView.text.toString()
+        binding.logView.text = if (currentLog == "Logs...") {
+            message
+        } else {
+            "$currentLog\n$message"
         }
     }
 }
